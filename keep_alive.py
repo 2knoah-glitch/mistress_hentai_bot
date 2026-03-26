b21ed2cff94a9a193ef2ace7d6fd6a14d86148c8
Fortune army <dontbannedme96@gmail.com>
2026-03-26 13:19:09 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,19 @@
 
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Your Mistress Bot is alive."
+
+def run():
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
